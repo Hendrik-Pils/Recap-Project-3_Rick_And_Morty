7f3f11943254dc1277b6f4718d7f63ab86e068ff
Hendrik Pils <hendrik-pils@gmx.de>
2023-11-30 09:54:42 +0100

    apply bugfixes

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,6 +1,6 @@
-export function CharacterCard({image, alt, name, status, type, episode}) {
-    const cardContainer = document.querySelector('[data-js="card-container"]');
-    cardContainer.innerHTML += `<li class="card">
+export function CharacterCard({ image, alt, name, status, type, episode }) {
+  const cardContainer = document.querySelector('[data-js="card-container"]');
+  cardContainer.innerHTML += `<li class="card">
     <div class="card__image-container">
       <img
         class="card__image"
@@ -12,12 +12,12 @@ export function CharacterCard({image, alt, name, status, type, episode}) {
     <div class="card__content">
       <h2 class="card__title">${name}</h2>
       <dl class="card__info">
-        <dt class="card__info-title">${status}</dt>
-        <dd class="card__info-description">Alive</dd>
-        <dt class="card__info-title">${type}</dt>
-        <dd class="card__info-description"></dd>
-        <dt class="card__info-title">${episode.length}</dt>
-        <dd class="card__info-description">51</dd>
+        <dt class="card__info-title">Status</dt>
+        <dd class="card__info-description">${status}</dd>
+        <dt class="card__info-title">Type</dt>
+        <dd class="card__info-description">${type}</dd>
+        <dt class="card__info-title">Occurences</dt>
+        <dd class="card__info-description">${episode.length}</dd>
       </dl>
     </div>
   </li>`;

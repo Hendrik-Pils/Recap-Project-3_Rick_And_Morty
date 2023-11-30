b699bdecdeffa605ca21c7ba13a2575f946e5bd1
Hendrik Pils <hendrik-pils@gmx.de>
2023-11-30 12:15:25 +0100

    bugfixes

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ import { buttonNext } from "./components/NavButton/NavButton.js";
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
+
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+const navigation = document.querySelector('[data-js="navigation"]');
 const searchBar = document.querySelector('[data-js="search-bar"]');
 
 // States
 const searchQuery = "";
+
+let page = 1;
+let maxPage = await fetchMaxPages().pages;
+
+console.log(maxPage);
 
 // ==============
 // API URL = "https://rickandmortyapi.com/api/character"
@@ -32,16 +41,32 @@ const searchQuery = "";
 // }
 // ==============
 
-async function fetchCharacters(pageIndex) {
+async function fetchMaxPages() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character`);
+  const json = await response.json();
+  return json.info;
+}
+
+async function fetchCharacters() {
   const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${pageIndex}`
+    `https://rickandmortyapi.com/api/character?page=${page}`
   );
   const json = await response.json();
   return json.results;
 }
 
-const characters = await fetchCharacters(page);
+const characters = await fetchCharacters();
 
 characters.forEach((character) => {
   CharacterCard(character);
+});
+
+nextButton.addEventListener("click", () => {
+  if (page >= maxPage) return;
+  page++;
+});
+
+prevButton.addEventListener("click", () => {
+  if (page <= 1) return;
+  page--;
 });

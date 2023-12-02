db1d3591a3539a1f64235b6c2041a2a7157bad17
markusgemeinder <190774@gmx.de>
2023-12-02 19:47:08 +0100

    Optimize handleSearchBar() in component SearchBar.js (only one handle function left)

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,27 +1,13 @@
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const searchBarInput = document.querySelector('[data-js="search-bar-input"]');
 
-let searchQuery = "";
-
-export function handleSearchBar() {
-  searchBar.addEventListener("submit", handleSearchInput); // This is optional, it handles form submission
-  searchBar.addEventListener("keydown", handleSearchEnterKey);
-  searchBar.addEventListener("click", handleSearchButton);
-}
-
-function handleSearchInput() {
-  searchQuery = searchBarInput.value;
-  console.log(searchQuery);
-}
-
-function handleSearchEnterKey(event) {
-  if (event.key === "Enter") {
-    event.preventDefault(); // Prevent the form from submitting (if it's inside a form)
-    handleSearchInput();
-  }
-}
-
-function handleSearchButton(event) {
-  event.preventDefault(); // Prevent the form from submitting (if it's inside a form)
-  handleSearchInput();
+export function handleSearchBar(callback) {
+  searchBar.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let searchQuery = searchBarInput.value;
+    console.log(searchQuery);
+    if (callback) {
+      callback(searchQuery);
+    }
+  });
 }

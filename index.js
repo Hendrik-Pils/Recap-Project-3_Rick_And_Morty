a05c3dcf030e2a40686a4b2f37653474874046bc
markusgemeinder <190774@gmx.de>
2023-12-02 09:48:13 +0100

    Update search bar (add event listeners for form, input and button; implement first functionality for handleEnterKey(event))

--- index.js
+++ index.js
@@ -2,21 +2,13 @@ import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 import { navPagination } from "./components/NavPagination/NavPagination.js";
 import { initializeNavButtons } from "./components/NavButton/NavButton.js";
 
+const navigation = document.querySelector('[data-js="navigation"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 
-// ==========================================
-// TODO: activate search bar (see README.md)
-// ==========================================
-
-const navigation = document.querySelector('[data-js="navigation"]');
-const searchBar = document.querySelector('[data-js="search-bar"]');
-const searchQuery = "";
-
-// ==========================================
-
 let page = 1;
+let searchQuery = "";
 
 async function fetchMaxPages() {
   const response = await fetch(
@@ -48,10 +40,36 @@ async function renderCharacters(page) {
   navPagination(page, maxPage);
 }
 
-renderCharacters(page);
-
 const { prevButton, nextButton } = initializeNavButtons(
   page,
   maxPage,
   renderCharacters
 );
+
+renderCharacters(page);
+
+// ================CODE FIX ABOVE==========================
+
+// ==========================================
+// TODO: activate search bar (see README.md)
+// ==========================================
+
+const searchBar = document.querySelector('[data-js="search-bar"]');
+const searchBarInput = document.querySelector('[data-js="search-bar-input"]');
+const searchBarButton = document.querySelector('[data-js="search-bar-button"]');
+
+function handleSearch() {
+  searchQuery = searchBarInput.value;
+  console.log(searchQuery);
+}
+
+function handleEnterKey(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Prevent the form from submitting (if it's inside a form)
+    handleSearch();
+  }
+}
+
+searchBarButton.addEventListener("click", handleSearch);
+searchBar.addEventListener("submit", handleSearch); // This is optional, it handles form submission
+searchBarInput.addEventListener("keydown", handleEnterKey);

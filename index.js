79d2c67d5699ca4aadebb2c051732956c674db4d
markusgemeinder <190774@gmx.de>
2023-12-03 12:15:46 +0100

    Update (outsource url in const, prepare code for additional urlAppendix ?name)

--- index.js
+++ index.js
@@ -3,51 +3,71 @@ import { navPagination } from "./components/NavPagination/NavPagination.js";
 import { initializeNavButtons } from "./components/NavButton/NavButton.js";
 import { handleSearchBar } from "./components/SearchBar/SearchBar.js";
 
+//======================================
+
 const navigation = document.querySelector('[data-js="navigation"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 
-let page = 1;
+const url = "https://rickandmortyapi.com/api/character";
+// let urlAppendix = `?page=${page}`;
+
+//======================================
 
 async function fetchMaxPages() {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${page}`
+  try {
+    const response = await fetch(`${url}`);
+    const json = await response.json();
+    // console.log(json);
+    return json.info.pages;
+  } catch (error) {
+    console.error("Error fetching max pages:", error);
+  }
+}
+(async () => {
+  const maxPage = await fetchMaxPages();
+  let page = 1;
+  //======================================
+
+  async function fetchCharacters(page) {
+    try {
+      const response = await fetch(`${url}?page=${page}`);
+      const json = await response.json();
+      // console.log(json);
+      return json.results;
+    } catch (error) {
+      console.error("Error fetching characters:", error);
+    }
+  }
+
+  //======================================
+
+  async function renderCharacters(page) {
+    const cardContainer = document.querySelector('[data-js="card-container"]');
+    cardContainer.innerHTML = "";
+
+    const characters = await fetchCharacters(page);
+
+    characters.forEach((character) => {
+      CharacterCard(character);
+    });
+    navPagination(page, maxPage);
+  }
+
+  //======================================
+
+  const { prevButton, nextButton } = initializeNavButtons(
+    page,
+    maxPage,
+    renderCharacters
   );
-  const json = await response.json();
-  return json.info.pages;
-}
 
-const maxPage = await fetchMaxPages();
+  //======================================
 
-async function fetchCharacters(page) {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${page}`
-  );
-  const json = await response.json();
-  return json.results;
-}
-
-async function renderCharacters(page) {
-  const cardContainer = document.querySelector('[data-js="card-container"]');
-  cardContainer.innerHTML = "";
-
-  const characters = await fetchCharacters(page);
-
-  characters.forEach((character) => {
-    CharacterCard(character);
-  });
-  navPagination(page, maxPage);
-}
-
-const { prevButton, nextButton } = initializeNavButtons(
-  page,
-  maxPage,
-  renderCharacters
-);
-
-handleSearchBar();
-renderCharacters(page);
+  handleSearchBar();
+  renderCharacters(page);
+})();
 
 // ==========================================
 // TODO: add functions to handleSearchBar()
